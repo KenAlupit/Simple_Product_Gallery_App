e25ea67471844a1e4336bb3f71ed1ba2d94b1994
alabasoangelo <dean.alabaso@ciit.edu.ph>
2024-10-08 08:56:51 +0800

    Initial Design with pop up

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,180 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Product Gallery',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Product Gallery'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        children: const [
+          ProductCard(
+            title: 'Product 1',
+            imageUrl:
+                'https://dummyimage.com/300x150/FF5733/FFFFFF&text=Product+1',
+            price: 29.99,
+            description: 'This is a description for Product 1.',
+          ),
+          ProductCard(
+            title: 'Product 2',
+            imageUrl:
+                'https://dummyimage.com/300x150/33FF57/FFFFFF&text=Product+2',
+            price: 49.99,
+            description: 'This is a description for Product 2.',
+          ),
+          ProductCard(
+            title: 'Product 3',
+            imageUrl:
+                'https://dummyimage.com/300x150/3357FF/FFFFFF&text=Product+3',
+            price: 19.99,
+            description: 'This is a description for Product 3.',
+          ),
+          ProductCard(
+            title: 'Product 4',
+            imageUrl:
+                'https://dummyimage.com/300x150/FF33A1/FFFFFF&text=Product+4',
+            price: 39.99,
+            description: 'This is a description for Product 4.',
+          ),
+          ProductCard(
+            title: 'Product 5',
+            imageUrl:
+                'https://dummyimage.com/300x150/FFC300/FFFFFF&text=Product+5',
+            price: 24.99,
+            description: 'This is a description for Product 5.',
+          ),
+          ProductCard(
+            title: 'Product 6',
+            imageUrl:
+                'https://dummyimage.com/300x150/DAF7A6/FFFFFF&text=Product+6',
+            price: 59.99,
+            description: 'This is a description for Product 6.',
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ProductCard extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final double price;
+  final String description; // New parameter
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+    required this.price,
+    required this.description, // New parameter
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return GestureDetector(
+      onTap: () {
+        _showProductDialog(context);
+      },
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              imageUrl,
+              height: 100,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Center(
+                    child: Text('Image not found',
+                        style: TextStyle(color: Colors.red)));
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                '\$${price.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16, color: Colors.green),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _showProductDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                imageUrl,
+                height: 150,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Center(
+                      child: Text('Image not found',
+                          style: TextStyle(color: Colors.red)));
+                },
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '\$${price.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16, color: Colors.green),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                description, // Displaying the product description
+                style: const TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
